be6f491c3d5f7e76fb77b9726e6c786d76552fcc
Administrator <haha>
2020-12-09 23:30:13 +0800

    优化：优化版本

--- test/pro.py
+++ test/pro.py
@@ -1,45 +1,22 @@
-import sys
+from __future__ import print_function
 import cv2 as cv
-
-
-def main(argv):
-    print("""
-    Zoom In-Out demo
-    ------------------
-    * [i] -> Zoom [i]n
-    * [o] -> Zoom [o]ut
-    * [ESC] -> Close program
-    """)
-
-    filename = argv[0] if len(argv) > 0 else 'img/1.jpg'
-    # Load the image
-    src = cv.imread(cv.samples.findFile(filename))
-    # Check if image is loaded fine
-    if src is None:
-        print('Error opening image!')
-        print('Usage: pyramids.py [image_name -- default ../data/chicky_512.png] \n')
-        return -1
-
-    while 1:
-        rows, cols, _channels = map(int, src.shape)
-
-        cv.imshow('Pyramids Demo', src)
-
-        k = cv.waitKey(0)
-        if k == 27:
-            break
-
-        elif chr(k) == 'i':
-            src = cv.pyrUp(src, dstsize=(2 * cols, 2 * rows))
-            print('** Zoom In: Image x 2')
-
-        elif chr(k) == 'o':
-            src = cv.pyrDown(src, dstsize=(cols // 2, rows // 2))
-            print('** Zoom Out: Image / 2')
-
-    cv.destroyAllWindows()
-    return 0
-
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
+import numpy as np
+import argparse
+parser = argparse.ArgumentParser(description='Code for Feature Detection tutorial.')
+parser.add_argument('--input', help='Path to input image.', default='img/sk1.jpg')
+args = parser.parse_args()
+src = cv.imread(args.input, cv.IMREAD_GRAYSCALE)
+if src is None:
+    print('Could not open or find the image:', args.input)
+    exit(0)
+#-- Step 1: Detect the keypoints using SURF Detector
+minHessian = 400
+detector = cv.xfeatures2d_SURF.create(hessianThreshold=minHessian)
+keypoints = detector.detect(src)
+#-- Draw keypoints
+img_keypoints = np.empty((src.shape[0], src.shape[1], 3), dtype=np.uint8)
+cv.drawKeypoints(src, keypoints, img_keypoints)
+print(keypoints)
+#-- Show detected (drawn) keypoints
+cv.imshow('SURF Keypoints', img_keypoints)
+cv.waitKey()
